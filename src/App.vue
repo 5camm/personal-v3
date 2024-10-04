f37e90bb476592603bb90069195d514fe2c9b9a9
Cam <5cam@proton.me>
2024-10-04 17:13:09 +0000

    :heavy_plus_sign: starter foundation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav-bar></nav-bar>
+    <main class="min-h-screen flex flex-col pt-16 px-2">
+      <router-view :key="route.fullPath" v-slot="{ Component }">
+        <suspense>
+          <component :is="Component" />
+          <template #fallback>
+            <loader></loader>
+          </template>
+        </suspense>
+      </router-view>
+    </main>
+  </template>
+  
+  <script setup>
+  import { useRoute } from 'vue-router'
+  import Loader from './components/Loader.vue'
+  import NavBar from './components/NavBar.vue'
+  
+  const route = useRoute()
+  </script>
+  
+  <style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+  </style>
